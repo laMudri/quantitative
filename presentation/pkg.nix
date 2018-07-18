41362de13d92c8c6d0fc6ba979e80e039cd9ce11
laMudri <lamudri@gmail.com>
2018-07-18 17:17:55 +0100

    start on configuration slide

--- presentation/pkg.nix
+++ presentation/pkg.nix
@@ -2,7 +2,7 @@
 let
   tex-env = texlive.combine {
     inherit (texlive) scheme-small latexmk beamer stmaryrd mathpartir
-                      cmll xcolor paralist makecell;
+                      cmll xcolor paralist makecell tikz-cd;
   };
 in stdenv.mkDerivation {
   name = "quantitative-presentation";
