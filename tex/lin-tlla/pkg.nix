0b96d1534f4e72151526ad242f48b59fc88b42a3
laMudri <lamudri@gmail.com>
2020-03-23 12:37:15 +0000

    [ progress ] adapt subst-note to eptcs style

---
+++ tex/lin-tlla/pkg.nix
@@ -0,0 +1,15 @@
+{ stdenv, texlive }:
+let
+  tex-env = texlive.combine {
+    inherit (texlive) scheme-small latexmk beamer stmaryrd mathpartir rsfs
+                      cmll xcolor paralist makecell tikz-cd ncctools;
+  };
+in stdenv.mkDerivation {
+  name = "lin-ttla";
+  src = ./.;
+  buildInputs = [ tex-env ];
+  buildPhase = ''
+    latexmk quant-subst.tex
+  '';
+  installPhase = "";
+}
